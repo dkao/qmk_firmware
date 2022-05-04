bc6e11431313077c4eed239f562acda9b2c1a80d
Daniel Kao <daniel.m.kao@gmail.com>
2022-05-03 21:26:48 -0700

    Drop Alt: Customizations
    * Turn off obnoxious LEDs
    * 1 KHz polling
    * Layer tap changes
    * Debounce strategy selection
    * Keymap

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -127,4 +127,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_LED_PROCESS_LIMIT 15
 #define RGB_MATRIX_LED_FLUSH_LIMIT 10
 
+#define HOLD_ON_OTHER_KEY_PRESS
+#define TAPPING_FORCE_HOLD
+
+#define USB_POLLING_INTERVAL_MS 1
+
 #include "config_led.h"
