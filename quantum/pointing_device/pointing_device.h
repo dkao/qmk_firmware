ba1bfa4110506afe985271d66d08b0d67ea7111d
Alabastard-64 <96358682+Alabastard-64@users.noreply.github.com>
2022-09-24 21:11:11 -0600

    manual linting attempt

--- quantum/pointing_device/pointing_device.h
+++ quantum/pointing_device/pointing_device.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "host.h"
 #include "report.h"
 
-
 #ifdef POINTING_DEVICE_SCROLL_ENABLE
 #    include "pointing_device_scroll.h"
 #endif
