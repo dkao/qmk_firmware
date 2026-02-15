bf71d5b17430c9df79971818a4c4aab8245c967f
Daniel Kao <daniel.m.kao@gmail.com>
2026-02-14 16:47:09 -0800

    RPG (WASD) layers for Dactyl Manuform

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/dkao/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/dkao/keymap.c
@@ -54,9 +54,11 @@
 #define _RAISE 3
 #define _LOWER 4
 #define _UTILITY 5
+#define _SHMUP 6
 #define _FPS1 7
 #define _FPS2 8
-#define _SHMUP 6
+#define _RPG1 9
+#define _RPG2 10
 #elif 0
 #define _QWERTY 1
 #define _RAISE 2
@@ -92,6 +94,8 @@
 #define UTILITY MO(_UTILITY)
 #define FPS1 TO(_FPS1)
 #define FPS2 MO(_FPS2)
+#define RPG1 TO(_RPG1)
+#define RPG2 MO(_RPG2)
 #define SHMUP TO(_SHMUP)
 #define RETB TO(_BASE)
 #if 1
@@ -384,7 +388,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_UTILITY] = LAYOUT_fix(
         QWERTY,  XXXXXXX, XXXXXXX, XXXXXXX, FPS1,                                         SHMUP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ENGRAM,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ENGRAM,                                       RPG1,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RETB,                                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                  QK_BOOT, XXXXXXX,                                                                          XXXXXXX, QK_BOOT,
                                    XXXXXXX, XXXXXXX,                                      XXXXXXX, XXXXXXX,
@@ -480,6 +484,66 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_ESC,  KC_UP,                                        KC_LSFT, KC_ENT,
                                                      KC_SPC,  RETB,     RETB,    XXXXXXX,
                                                      XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX
+    ),
+
+    /* RPG 1
+     * ,----------------------------------,                             ,----------------------------------,
+     * |      |      |      |      |      |                             |      |      |      |      |      |
+     * |------+------+------+------+------|                             |-------------+------+------+------|
+     * |      |      |      |      |      |                             |      |      |      |      |      |
+     * |------+------+------+------+------|                             |------|------+------+------+------|
+     * |      |      |      |      |      |                             |      |      |      |      |      |
+     * |------+------+------+-------------,                             ,-------------+------+------+------,
+     *        | RESET|      |                                                         |      | RESET|
+     *        '------+------'-------------'                             '-------------'------+------'
+     *                      |      |      |                             |      |      |
+     *                      |      |      |                             |      |      |
+     *                      |      |      |                             |      |      |
+     *                      '------+------'                             '------+------'
+     *                                    '------+------' '------+------'
+     *                                    |      |      | |      |      |
+     *                                    '------+------' '------+------'
+     *                                    |      |      | |      |      |
+     *                                    '------+------' '------+------'
+     */
+    [_RPG1] = LAYOUT_fix(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                                         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                                         KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                                         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,
+                 KC_LALT, KC_BSPC,                                                                          KC_LBRC, KC_RBRC,
+                                   KC_ESC,  KC_SPC,                                       RPG2,    KC_ENT,
+                                                     KC_1,    KC_3,     RETB,    KC_P,
+                                                     KC_2,    KC_4,     KC_F8,   KC_F5
+    ),
+
+    /* RPG 2
+     * ,----------------------------------,                             ,----------------------------------,
+     * |      |      |      |      |      |                             |      |      |      |      |      |
+     * |------+------+------+------+------|                             |-------------+------+------+------|
+     * |      |      |      |      |      |                             |      |      |      |      |      |
+     * |------+------+------+------+------|                             |------|------+------+------+------|
+     * |      |      |      |      |      |                             |      |      |      |      |      |
+     * |------+------+------+-------------,                             ,-------------+------+------+------,
+     *        | RESET|      |                                                         |      | RESET|
+     *        '------+------'-------------'                             '-------------'------+------'
+     *                      |      |      |                             |      |      |
+     *                      |      |      |                             |      |      |
+     *                      |      |      |                             |      |      |
+     *                      '------+------'                             '------+------'
+     *                                    '------+------' '------+------'
+     *                                    |      |      | |      |      |
+     *                                    '------+------' '------+------'
+     *                                    |      |      | |      |      |
+     *                                    '------+------' '------+------'
+     */
+    [_RPG2] = LAYOUT_fix(
+        _______, _______, _______, _______, _______,                                      KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,
+        _______, _______, _______, _______, _______,                                      KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
+        _______, _______, _______, _______, _______,                                      KC_9,    KC_0,    KC_SCLN, KC_QUOT, KC_SLSH,
+                 _______, _______,                                                                          KC_MINS, KC_EQL,
+                                   _______, _______,                                      _______, _______,
+                                                     KC_5,    KC_7,     _______, KC_GRV,
+                                                     KC_6,    KC_8,     _______, _______
     ),
 };
 
@@ -718,7 +782,7 @@ void debounce_free(void) {
 
 bool debounce(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, bool changed) {
     bool updated_last = false;
-    bool use_eager = (IS_LAYER_ON(_SHMUP) || IS_LAYER_ON(_FPS1) || IS_LAYER_ON(_FPS2));
+    bool use_eager = (IS_LAYER_ON(_SHMUP) || IS_LAYER_ON(_FPS1) || IS_LAYER_ON(_FPS2) || IS_LAYER_ON(_RPG1) || IS_LAYER_ON(_RPG2));
     cooked_changed    = false;
 
     if (use_eager != last_eager) {
